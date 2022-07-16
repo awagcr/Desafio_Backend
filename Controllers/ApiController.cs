b0571366be14626f69e4c76284b7d81f5ca74618
Alexandre Wagner <alexandrewcr@gmail.com>
2022-07-16 08:52:28 -0300

    Commit inicial

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,39 @@
+﻿using Business;
+using Business.API;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Desafio_Backend_Aliare.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ApiController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<ApiController> _logger;
+
+        public ApiController(ILogger<ApiController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("cidade")]
+        public string ObtenhaPlaylistCidade(string cidade) => new ProcessoDeConsultaPorCidade().Obtenha(cidade);
+
+        [HttpGet]
+        [Route("coordenada")]
+        public string ObtenhaPlaylistCoordenada(int latitute, int longitute)
+        {
+
+        }
+    }
+}
